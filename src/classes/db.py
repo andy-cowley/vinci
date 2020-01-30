c500b0c5f65d7e083c461a6f30f850306d87c88a
Andy Cowley <andy.cowley@five.ai>
2020-01-30 19:53:24 +0000

    Rudimentary flask app and basic template; returns tag index

--- src/classes/db.py
+++ src/classes/db.py
@@ -4,7 +4,7 @@ import sqlite3
 class DBConnectionHandler:
     def __init__(self, db_file):
         try:
-            self.con = sqlite3.connect(db_file)
+            self.con = sqlite3.connect(db_file, check_same_thread=False)
             cursor = self.con.cursor()
             print("Database created and Successfully Connected to SQLite")
 
--- src/main.py
+++ src/main.py
@@ -1,87 +1,47 @@
 import os
 
-from classes import DBConnectionHandler, MDFile
-from pathlib import Path, PurePath
+from classes import DBConnectionHandler
+from functions import create_markdown_list, write_default_metadata, update_database, create_tag_index
+from flask import Flask, flash, make_response, send_file, redirect, request, render_template, url_for
 
 
-def create_markdown_list(path_string):
-    """Returns a list of MDFile objects from markdown files recursively"""
-    p = Path(path_string)
-    all_markdown = list(p.glob("**/*.md"))
-    all_markdown_as_string = [str(PurePath(x)) for x in all_markdown]
-    markdown_file_list = [MDFile(x) for x in all_markdown_as_string]
-    return markdown_file_list
+# -----------------------------------------------------------------------------
+DB_FILE = os.getenv("DB_FILE")
+
+if not DB_FILE:
+    DB_FILE = "sqlite_debug.db"
+
+db = DBConnectionHandler(DB_FILE)
+
+update_database(db)
+
+write_default_metadata()
 
 
-def write_default_metadata(path="."):
-    markdown_file_list = create_markdown_list(path)
-    [md_file.write() for md_file in markdown_file_list]
+db.commit()
+#    select_string = """
+#    SELECT notes.name, tags.tag
+#    FROM notes
+#    JOIN notes_tags
+#    ON notes.id = notes_tags.note
+#    JOIN tags
+#    ON tags.id = notes_tags.tag
+#    """
+
+#    db.execute(select_string)
+#    results = db.cursor.fetchall()
+
+#    [print(result) for result in results]
+
+app = Flask(__name__)
+app.secret_key = "This key is required for `flash()`."
 
 
-def update_database(db_connection, path="."):
-    markdown_file_list = create_markdown_list(path)
-
-    for md_file in markdown_file_list:
-        select_string = f"SELECT id FROM notes WHERE notes.path = '{md_file.file_obj}'"
-        db_connection.execute(select_string)
-        note_index = db_connection.cursor.fetchone()[0]
-
-        if not note_index:
-            insert_string = f"""
-            INSERT INTO notes (name,author,path)
-            VALUES(
-            '{md_file.md.metadata['title']}',
-            '{md_file.md.metadata['author']}',
-            '{md_file.file_obj}')
-            """
-            db_connection.execute(insert_string)
-
-        for tag in md_file.md.metadata["tags"]:
-            select_tag_string = f"SELECT id FROM tags WHERE tags.tag = '{tag}'"
-            db_connection.execute(select_tag_string)
-            tag_index = db_connection.cursor.fetchone()[0]
-
-            if not tag_index:
-                insert_string = f" INSERT INTO tags (tag) VALUES ('{tag}')"
-                print(insert_string)
-                db_connection.execute(insert_string)
-
-            select_join_string = f"""
-            SELECT note,tag FROM notes_tags WHERE note = '{note_index}' AND tag = '{tag_index}'
-            """
-            db_connection.execute(select_join_string)
-            join = db_connection.cursor.fetchone()
-
-            if not join:
-                insert_string = f"INSERT INTO notes_tags (note,tag) VALUES ('{note_index}','{tag_index}')"
-                db_connection.execute(insert_string)
+@app.route("/", methods=["GET"])
+def index():
+    tag_index = create_tag_index(db)
+    tag_index_tuple_sum = sum([tag[1] for tag in tag_index])
+    return render_template("index.html", tag_index_tuple=tag_index, tag_index_tuple_sum=tag_index_tuple_sum)
 
 
-if __name__ == "__main__":
-    DB_FILE = os.getenv("DB_FILE")
-
-    if not DB_FILE:
-        DB_FILE = "sqlite_debug.db"
-
-    db = DBConnectionHandler(DB_FILE)
-
-    update_database(db)
-
-    write_default_metadata()
-
-    select_string = """
-    SELECT notes.name, tags.tag
-    FROM notes
-    JOIN notes_tags
-    ON notes.id = notes_tags.note
-    JOIN tags
-    ON tags.id = notes_tags.tag
-    """
-
-    db.execute(select_string)
-    results = db.cursor.fetchall()
-
-    [print(result) for result in results]
-
-    db.commit()
-    db.close()
+app.run(debug=True, host="0.0.0.0", port=5000)
